4ede6d073aa33bac1485ec801ff55cc07b7e4856
iv.pimenov <ivan.pimenov@fabit.ru>
2022-06-26 19:35:25 +0700

    homework#4, function

---
+++ homework4.py
@@ -0,0 +1,22 @@
+def open_browser(browser_name):
+    pass
+
+
+def go_to_companyname_homepage(page_url):
+    pass
+
+
+def find_registration_button_on_login_page():
+    pass
+
+functions = [open_browser, go_to_companyname_homepage, find_registration_button_on_login_page]
+
+def printing_name_and_args(args):
+    for arg in args:
+        print("Имя функции:", arg.__name__.capitalize().replace("_", " "), end='. ')
+        if not any(arg.__code__.co_varnames):
+            print("Аргументы отстуствуют")
+        else:
+            print("Аргумент(ы) функции:", arg.__code__.co_varnames)
+
+printing_name_and_args(functions)
