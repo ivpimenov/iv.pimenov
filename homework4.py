07d32f530eee07d41371abcfbf52c87ee71366c4
iv.pimenov <ivan.pimenov@fabit.ru>
2022-06-27 12:02:37 +0700

    hw4 fixedd

--- homework4.py
+++ homework4.py
@@ -1,16 +1,3 @@
-def open_browser(browser_name):
-    pass
-
-
-def go_to_companyname_homepage(page_url):
-    pass
-
-
-def find_registration_button_on_login_page():
-    pass
-
-function = [open_browser, go_to_companyname_homepage, find_registration_button_on_login_page]
-
 def printing_name_and_args(args):
     for arg in args:
         print("Имя функции:", arg.__name__, end='. ')
@@ -19,7 +6,24 @@ def printing_name_and_args(args):
         else:
             print("Аргумент(ы) функции:", arg.__code__.co_varnames)
 
-printing_name_and_args(function)
+
+def open_browser(browser_name):
+    pass
+
+def go_to_companyname_homepage(page_url):
+    pass
+
+def find_registration_button_on_login_page():
+    pass
+
+functions = [open_browser, go_to_companyname_homepage, find_registration_button_on_login_page]
+
+printing_name_and_args(functions)
+
+
+
+
+
 
 
 
