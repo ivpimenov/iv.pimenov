6c42e4b51cac72960b4e01c4aac30a8875ba1afb
iv.pimenov <ivan.pimenov@fabit.ru>
2022-06-27 11:48:18 +0700

    hw4 fixed

--- homework4.py
+++ homework4.py
@@ -13,10 +13,14 @@ functions = [open_browser, go_to_companyname_homepage, find_registration_button_
 
 def printing_name_and_args(args):
     for arg in args:
-        print("Имя функции:", arg.__name__.capitalize().replace("_", " "), end='. ')
+        print("Имя функции:", arg.__name__, end='. ')
         if not any(arg.__code__.co_varnames):
             print("Аргументы отстуствуют")
         else:
             print("Аргумент(ы) функции:", arg.__code__.co_varnames)
 
 printing_name_and_args(functions)
+
+
+
+
